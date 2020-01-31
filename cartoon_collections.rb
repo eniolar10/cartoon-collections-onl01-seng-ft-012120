22b23b3574ed6a03a0b21ca116eaf3491e62e102
Ola Enol <eniolar10@gmail.com>
2020-01-31 18:21:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,16 +7,18 @@ end
  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet (planeteers)# code an argument here
  planeteers.map do|planeteer|# Your code here
- puts "#{planeteer_calls}!"
+ puts "#{planeteer_calls}!.upcase"
  end
  
 end
 
-
+short_words = ["puff", "go", "two"]
 def long_planeteer_calls (short_words)# code an argument here
-  # Your code here
+  short_words.each do |short_word|
+    if {|short_words.length >4|}# Your code here
+  short_words = false
 end
-
+end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
